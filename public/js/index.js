4dc689d44b37427e3e3f489dbe54a02d8e234d08
Fikri Wahidin <fikriayam4@gmail.com>
2023-05-20 18:26:59 +0700

    merapikan folder dan menambahkan tailwindcss

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('sudah-muncul');
+        } else {
+            entry.target.classList.remove('sudah-muncul');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.belum-muncul');
+hiddenElements.forEach((element) => observer.observe(element));
